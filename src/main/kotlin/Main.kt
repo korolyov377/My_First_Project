9600fc7ec60c8ed58e9ce34fa152b14956748536
unknown <c:\the bat!>
2024-09-03 11:09:46 +0300

    My First commit 4

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    /*Вывод текста
+    * в
+    * несколько
+    * строк */
+    println("Hello")
+    println("Kotlin")
+
+    //Вывод в одну строку
+    /*print("Hello")
+    print("Kotlin")*/
+}
